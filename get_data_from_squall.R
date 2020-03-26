530fba3e1711b094c7808f3b51cfb4f594a256d1
ea-guerette <ea-guerette@users.noreply.github.com>
2020-03-26 12:14:53 +1100

    Added more code to get C3, R1 data, also rename SHIM data

--- get_data_from_squall.R
+++ get_data_from_squall.R
@@ -33,10 +33,10 @@ library(odbc)
 con <- dbConnect(odbc::odbc(), 
                  .connection_string = 'driver={SQL Server};server=squall-cdc.it.csiro.au;database=gaslab;trusted_connection=true')
 #this also works:
-#con <- dbConnect(odbc::odbc(), "gaslab") #this is using a DSN
+#con <- dbConnect(odbc::odbc(), "gaslab") #this is using a DSN - had to set it up in Windows 
 
 #using odbc() 
-dbListTables(con)
+dbListTables(con) #this lists all tables in the database
 #data <- dbReadTable(con, "SHIM-1")
 #as_tibble(data)
 
@@ -49,16 +49,24 @@ dbListTables(con)
 #show_query(q1)
 
 q_shim <- tbl(con, "SHIM-1")  %>% ###aha this actually works, the list thing was throwing me off
-  select(- c(`Decimal Year`, `N2O ar mixing ratio`, `N2O ar mixing ratio sd`)) %>% 
-  filter(`N2O flag` == 0)#, `N2O # aliquots` >1) #probably can do this at a later stage (when calculating weighted means)
+  filter(`N2O flag` == 0) %>% 
+  select(- c(`Decimal Year`, `N2O flag`, `N2O ar mixing ratio`, `N2O ar mixing ratio sd`))  
 show_query(q_shim)
 
-shim_data <- collect(q_shim)
+shim_data <- collect(q_shim)  %>% rename(file = `File #`, date = `Date Started`, n = `N2O # aliquots`, N2O = `N2O ht mixing ratio`, sd = `N2O ht mixing ratio sd`) %>% 
+  mutate(instrument = "S1")
+
 
 q_c3 <- tbl(con, "CARLE-3")  %>% 
-  select(- c(`Decimal Year`, `CH4 ar mixing ratio`, `CH4 ar mixing ratio sd`, `CO2 ht mixing ratio`,`CO2 ht mixing ratio sd`)) %>% 
-  filter(`CO2 flag` == 0)
+  filter(`CO2 flag` == 0) %>% 
+  select(- c(`Decimal Year`, `CH4 flag`,`CH4 ar mixing ratio`, `CH4 ar mixing ratio sd`, `CO2 flag`,`CO2 ht mixing ratio`,`CO2 ht mixing ratio sd`))
+  
+c3_data <- collect(q_c3) %>% rename(file = `File #`, date = `Date Started`)
+#will rename more stuff once I know what I am doing
 
 q_r1 <- tbl(con, "RGA3-1")  %>% 
-  select(- c(`Decimal Year`, `H2 ar mixing ratio`, `H2 ar mixing ratio sd`, `CO ar mixing ratio`,`CO ar mixing ratio sd`)) %>% 
-filter(`CO flag` == 0)
+  filter(`CO flag` == 0) %>% 
+  select(- c(`Decimal Year`, `H2 flag`,`H2 ar mixing ratio`, `H2 ar mixing ratio sd`, `CO flag`,`CO ar mixing ratio`,`CO ar mixing ratio sd`))
+
+r1_data <- collect(q_r1) %>% rename(file = `File #`, date = `Date Started`)
+#will rename more stuff once I know what I am doing
